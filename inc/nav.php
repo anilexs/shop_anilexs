bc62f202d861ab774f8762a28f633bc7d9ca8e97
alexis <alexissimoes94@gmail.com>
2023-09-06 16:47:11 +0200

    v2

--- inc/nav.php
+++ inc/nav.php
@@ -2,8 +2,6 @@
 <body>
     <header>
         <?php if(isset($_SESSION['id'])){ ?>
-
-        <?php }else { ?>
             <nav>
                 <div>
                     <a href="http://localhost/shop_anilexs/accueil.php">
@@ -12,6 +10,26 @@
                 </div>
                 <div>
                     <button class="navmenu"></button>
+                </div>
+            </nav>
+        <?php }else { ?>
+            <nav>
+                <div>
+                    <a href="http://localhost/shop_anilexs/accueil.php">
+                        <img src="http://localhost/shop_anilexs/asset/img/shop.png" alt="" class="imgNav">
+                    </a>
+                </div>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+                <div>
+                    <a href="http://localhost/shop_anilexs/inscription.php"class="login">inscription</a>
+                    <span class="navslah">/</span>
+                    <a href="http://localhost/shop_anilexs/connexion.php" class="logout">connexion</a>
                 </div>
             </nav>
         <?php } ?>
